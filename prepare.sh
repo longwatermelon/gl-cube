23f51f4b5c25531b5587afed7eb80bede9adb986
longwatermelon <longwatermelon1@gmail.com>
2022-07-21 20:13:07 -0700

    prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+rm -rf .git
+rm -rf deps/cglm deps/glfw
+rm .gitmodules
+
+git init
+git add .
+git commit -m "first commit"
+
+git submodule add https://github.com/glfw/glfw deps/glfw
+git submodule add https://github.com/recp/cglm deps/cglm
+git add .
+git commit -m "deps"
+
